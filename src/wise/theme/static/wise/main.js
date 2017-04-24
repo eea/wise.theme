0e7150ea982a6eb265bedb07f6bb69068b5c3c28
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-04-24 18:52:18 +0300

    Refs #82725 Wise; use require to loads slick

--- src/wise/theme/static/wise/main.js
+++ src/wise/theme/static/wise/main.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
-
-$('.header-bg').slick({
-    arrows: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: false
+requirejs.config({
+  paths: {
+    'slick': ['//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min']
+  }
 });
 
-
-})
+require(['jquery', 'slick'], function($, slick) {
+    $(document).ready(function(){
+        $('.header-bg').slick({
+            arrows: true,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+          adaptiveHeight: false
+        });
+    });
+    return {};
+});
