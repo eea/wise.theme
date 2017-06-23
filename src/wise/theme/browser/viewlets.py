d82c5d70034dd55510835d7fed751666eaad9151
Petchesi-Iulian <iamnotacat12@gmail.com>
2017-06-23 17:09:44 +0300

    Refs #85253 WISE: Added diazo rule for analytics plugin.

--- src/wise/theme/browser/viewlets.py
+++ src/wise/theme/browser/viewlets.py
@@ -1,5 +1,6 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from tlspu.cookiepolicy.browser.viewlets import CookiePolicyViewlet
+from Products.Five.browser import BrowserView
 
 
 class CookiesViewlet(CookiePolicyViewlet):
@@ -7,3 +8,9 @@ class CookiesViewlet(CookiePolicyViewlet):
 
     def update(self):
         return super(CookiesViewlet, self).render()
+
+
+# class GoToPDB(BrowserView):
+#     def __call__(self):
+#         import pdb; pdb.set_trace()
+#         return "done"
