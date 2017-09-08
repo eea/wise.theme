90bc6765531507e87172483e3de99df433fc12fc
stefanvasilica <themacaneata@gmail.com>
2017-09-08 15:54:30 +0300

    Refs #87290 Wise; WIP - New menu implementation

--- src/wise/theme/static/wise/js/wise.js
+++ src/wise/theme/static/wise/js/wise.js
@@ -1,1 +1,1 @@
-define(["slick-carousel"],function(a){"use strict";function b(a){return a&&a.__esModule?a:{"default":a}}var c=(b(a),30),d=1;c+=d,console.log(c),document.addEventListener("DOMContentLoaded",function(){var a=30,b=1;a+=b,console.log(a);document.querySelector(".homepage .header-bg")})});
+define(["slick-carousel"],function(a){"use strict";function b(a){return a&&a.__esModule?a:{"default":a}}var c=(b(a),30),d=1;c+=d,console.log(c),document.addEventListener("DOMContentLoaded",function(){var a=30,b=1;a+=b,console.log(a),document.querySelector(".homepage .header-bg")})});
