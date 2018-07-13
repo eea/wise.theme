3f9a2aae3638bf5b4455a78e64ffa6ebf63f1567
ichim-david <ichim.david@gmail.com>
2018-07-13 15:43:21 +0300

    Refs #96264 wrap promo_slider call within jquery ready and fixed jsregistry condition

--- src/wise/theme/static/wise/src/js/promo_slider.js
+++ src/wise/theme/static/wise/src/js/promo_slider.js
@@ -6,15 +6,15 @@ if (window.matchMedia("(min-width: 800px)").matches) {
                 return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
             }
         });
-
-        var $hlSlider = $('#hlslider-slides'),
-            $hlSliderCounter = $('#hlslider-counter'),
-            $hlSliderPrev = $('#hlslider-prev'),
-            $hlSliderNext = $('#hlslider-next'),
-            $hlSliderPhotos = $('.highlight-photos').children();
-
-
         $(document).ready(function() {
+
+            var $hlSlider = $('#hlslider-slides'),
+                $hlSliderCounter = $('#hlslider-counter'),
+                $hlSliderPrev = $('#hlslider-prev'),
+                $hlSliderNext = $('#hlslider-next'),
+                $hlSliderPhotos = $('.highlight-photos').children();
+
+
             var $hlSliderPlaceholder = $('.highlight-placeholder');
             // $('#highlights-slider').prepend($hlSliderPlaceholder);
 
@@ -25,75 +25,76 @@ if (window.matchMedia("(min-width: 800px)").matches) {
             }).addClass('current');
             initSlider();
             $hlSliderPlaceholder.css('opacity', '0');
-        });
 
-        function initSlider() {
-            $hlSlider.children().css({ 'display': 'block' });
-            if (!$hlSlider.slick) {
-                return;
-            }
-            $hlSlider.slick({
-                autoplay: true,
-                autoplaySpeed: 6000,
-                speed: 1000,
-                easing: 'easeOutQuint',
-                adaptiveHeight: false,
-                nextArrow: '',
-                prevArrow: '',
-                useCSS: false
-            });
-
-            var updateBackgroundPhoto = function(current, next, count) {
-
-                var dir = next - current;
-                if (next === 0 && current === count - 1) {
-                    dir = 1;
+            function initSlider() {
+                $hlSlider.children().css({ 'display': 'block' });
+                if (!$hlSlider.slick) {
+                    return;
                 }
-                if (next === count - 1 && current === 0) {
-                    dir = -1;
-                }
-
-                var $currentImg = $($hlSliderPhotos[current]);
-                var $nextImg = $($hlSliderPhotos[next]);
-                $nextImg.show();
-
-                var percent = dir * 100 + '%';
-
-                $nextImg.css({
-                    'display': 'block',
-                    'transform': 'translate3d(' + percent + ', 0, 0)'
-                });
-
-                $({ 'percent': dir * 100 }).animate({
-                    'percent': 0
-                }, {
-                    duration: 1000,
+                $hlSlider.slick({
+                    autoplay: true,
+                    autoplaySpeed: 6000,
+                    speed: 1000,
                     easing: 'easeOutQuint',
-                    step: function(now) {
-                        $nextImg.css({
-                            'transform': 'translate3d(' + now + '%, 0, 0)'
-                        });
-                    },
-                    done: function() {
-                        $currentImg.removeClass('current').hide();
-                        $nextImg.addClass('current');
-                    }
+                    adaptiveHeight: false,
+                    nextArrow: '',
+                    prevArrow: '',
+                    useCSS: false
                 });
-            };
 
-            var i;
+                var updateBackgroundPhoto = function(current, next, count) {
 
-            $hlSlider.on('beforeChange', function(e, slick, currentSlide, nextSlide) {
-                i = (nextSlide ? nextSlide : 0) + 1;
-                $hlSliderCounter.text(i + '/' + slick.slideCount);
+                    var dir = next - current;
+                    if (next === 0 && current === count - 1) {
+                        dir = 1;
+                    }
+                    if (next === count - 1 && current === 0) {
+                        dir = -1;
+                    }
 
-                updateBackgroundPhoto(currentSlide, nextSlide, slick.slideCount);
-            });
+                    var $currentImg = $($hlSliderPhotos[current]);
+                    var $nextImg = $($hlSliderPhotos[next]);
+                    $nextImg.show();
 
-            $hlSliderNext.click(function() { $hlSlider.slick('slickNext'); });
-            $hlSliderPrev.click(function() { $hlSlider.slick('slickPrev'); });
+                    var percent = dir * 100 + '%';
 
-        }
+                    $nextImg.css({
+                        'display': 'block',
+                        'transform': 'translate3d(' + percent + ', 0, 0)'
+                    });
+
+                    $({ 'percent': dir * 100 }).animate({
+                        'percent': 0
+                    }, {
+                        duration: 1000,
+                        easing: 'easeOutQuint',
+                        step: function(now) {
+                            $nextImg.css({
+                                'transform': 'translate3d(' + now + '%, 0, 0)'
+                            });
+                        },
+                        done: function() {
+                            $currentImg.removeClass('current').hide();
+                            $nextImg.addClass('current');
+                        }
+                    });
+                };
+
+                var i;
+
+                $hlSlider.on('beforeChange', function(e, slick, currentSlide, nextSlide) {
+                    i = (nextSlide ? nextSlide : 0) + 1;
+                    $hlSliderCounter.text(i + '/' + slick.slideCount);
+
+                    updateBackgroundPhoto(currentSlide, nextSlide, slick.slideCount);
+                });
+
+                $hlSliderNext.click(function() { $hlSlider.slick('slickNext'); });
+                $hlSliderPrev.click(function() { $hlSlider.slick('slickPrev'); });
+
+            }
+
+        });
 
     }(jQuery))
 }
