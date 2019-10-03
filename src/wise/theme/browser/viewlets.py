e685b2a4eaaee10c35c7057a3890f91eb5ecb1c2
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-10-03 17:19:55 +0300

    Refactored easyslideshow implementation

--- src/wise/theme/browser/viewlets.py
+++ src/wise/theme/browser/viewlets.py
@@ -1,83 +1,112 @@
-from AccessControl import Unauthorized
-from plone import api
-from plone.app.layout.viewlets.common import GlobalSectionsViewlet
+from plone.api import content  # , portal
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.app.layout.viewlets import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from tlspu.cookiepolicy.browser.viewlets import CookiePolicyViewlet
 
 
-class CookiesViewlet(CookiePolicyViewlet):
-    render = ViewPageTemplateFile("pt/cookiepolicy.pt")
+class SlidesViewlet(ViewletBase):
 
-    def update(self):
-        return super(CookiesViewlet, self).render()
+    render = ViewPageTemplateFile("pt/slideshow.pt")
+
+    # def render(self):
+    #     import pdb
+    #     pdb.set_trace()
+    #
+    #     if INavigationRoot.providedBy(self.context):
+    #         return self.index()
+    #
+    #     else:
+    #         return ''
+
+    def getImages(self):
+
+        path = {'query': 'homepage-slide-images', 'depth': 1}
+        results = content.find(
+            path=path, portal_type='Image', sort_on='getObjPositionInParent')
+
+        return results
 
 
-class NavigationViewlet(GlobalSectionsViewlet):
-    """ Navigation menu viewlet override
-    """
-    index = ViewPageTemplateFile('pt/sections.pt')
-
-    def tabs(self):
-        root = api.portal.get_navigation_root(context=self.context)
-        contentish = ['Folder', 'Collection', 'Topic']
-        # tabs = [{
-        #     'url': root.absolute_url(),
-        #     'id': root.id,
-        #     'name': 'Home',
-        #     'image': '',
-        #     'subtabs': []}]
-        tabs = []
-
-        brains = root.getFolderContents(
-            contentFilter={
-                'portal_type': contentish
-
-            })
-
-        brains = [b for b in brains if b.exclude_from_nav is False]
-
-        for brain in brains:
-            obj = brain.getObject()
-            children = []
-
-            types = ['collective.cover.content', 'Document',
-                     'News Item', 'Event', 'Folder']
-            folder_contents = obj.getFolderContents(
-                contentFilter={
-                    'portal_type': types
-                })
-
-            for child in folder_contents:
-                if child.exclude_from_nav:
-                    continue
-                try:
-                    child = child.getObject()
-                except Unauthorized:
-                    continue
-                children.append({
-                    'url': child.absolute_url(),
-                    'description': '',
-                    'name': child.title,
-                    'id': child.id})
-
-            image = self.get_image(obj)
-            tab = {
-                'url': obj.absolute_url(),
-                'description': '',
-                'name': obj.title,
-                'id': obj.id,
-                'image': image,
-                'subtabs': children
-            }
-            tabs.append(tab)
-
-        return tabs
-
-    def get_image(self, obj):
-        if not hasattr(obj, 'image') or obj.image is None:
-            return ''
-
-        scales = obj.restrictedTraverse('@@images')
-        image = scales.scale('image', scale='menu-icon')
-
-        return image and image.absolute_url() or ''
+# from AccessControl import Unauthorized
+# from plone import api
+# from plone.app.layout.viewlets.common import GlobalSectionsViewlet
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+# from tlspu.cookiepolicy.browser.viewlets import CookiePolicyViewlet
+#
+#
+# class CookiesViewlet(CookiePolicyViewlet):
+#     render = ViewPageTemplateFile("pt/cookiepolicy.pt")
+#
+#     def update(self):
+#         return super(CookiesViewlet, self).render()
+#
+#
+# class NavigationViewlet(GlobalSectionsViewlet):
+#     """ Navigation menu viewlet override
+#     """
+#     index = ViewPageTemplateFile('pt/sections.pt')
+#
+#     def tabs(self):
+#         root = api.portal.get_navigation_root(context=self.context)
+#         contentish = ['Folder', 'Collection', 'Topic']
+#         # tabs = [{
+#         #     'url': root.absolute_url(),
+#         #     'id': root.id,
+#         #     'name': 'Home',
+#         #     'image': '',
+#         #     'subtabs': []}]
+#         tabs = []
+#
+#         brains = root.getFolderContents(
+#             contentFilter={
+#                 'portal_type': contentish
+#
+#             })
+#
+#         brains = [b for b in brains if b.exclude_from_nav is False]
+#
+#         for brain in brains:
+#             obj = brain.getObject()
+#             children = []
+#
+#             types = ['collective.cover.content', 'Document',
+#                      'News Item', 'Event', 'Folder']
+#             folder_contents = obj.getFolderContents(
+#                 contentFilter={
+#                     'portal_type': types
+#                 })
+#
+#             for child in folder_contents:
+#                 if child.exclude_from_nav:
+#                     continue
+#                 try:
+#                     child = child.getObject()
+#                 except Unauthorized:
+#                     continue
+#                 children.append({
+#                     'url': child.absolute_url(),
+#                     'description': '',
+#                     'name': child.title,
+#                     'id': child.id})
+#
+#             image = self.get_image(obj)
+#             tab = {
+#                 'url': obj.absolute_url(),
+#                 'description': '',
+#                 'name': obj.title,
+#                 'id': obj.id,
+#                 'image': image,
+#                 'subtabs': children
+#             }
+#             tabs.append(tab)
+#
+#         return tabs
+#
+#     def get_image(self, obj):
+#         if not hasattr(obj, 'image') or obj.image is None:
+#             return ''
+#
+#         scales = obj.restrictedTraverse('@@images')
+#         image = scales.scale('image', scale='menu-icon')
+#
+#         return image and image.absolute_url() or ''
--- src/wise/theme/interfaces.py
+++ src/wise/theme/interfaces.py
@@ -1,30 +1,15 @@
 # -*- coding: utf-8 -*-
+
 """Module where all interfaces, events and exceptions live."""
 
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from zope.interface import Interface
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class IWiseThemeLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
 
-class ISlideshowFolder(Interface):
-    """Content type interface for slideshows
-    """
-
-class ISlideshowWillBeCreatedEvent(Interface):
-    """An event that is fired before a child site is created
-    """
-
-class ISlideshowCreatedEvent(Interface):
-    """An event that is fired after a child site is created
-    """
-
-class ISlideshowWillBeRemovedEvent(Interface):
-    """An event that is fired before the child site is removed
-    """
-
-class ISlideshowRemovedEvent(Interface):
-    """An event that is fired after a child site is removed
+class IHomepage(Interface):
+    """ Marker interface for the context object that is the homepage
     """
