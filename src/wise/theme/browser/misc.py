4e30bc3b4b5a3002b395e5008bfe418942cddde5
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2019-10-31 15:48:56 +0200

    Remove unused code

--- src/wise/theme/browser/misc.py
+++ src/wise/theme/browser/misc.py
@@ -1,30 +1,6 @@
 from plone.api import content, portal
 from Products.Five.browser import BrowserView
 
-from ..interfaces import IFullWidthLayout
-
-
-class FrontpageSlidesView (BrowserView):
-    """ BrowserView for the frontpage slides which will be loaded through diazo
-    """
-
-    def __call__(self):
-        site = portal.get()
-        sf = site.unrestrictedTraverse('marine/frontpage-slides')
-        self.images = [o for o in sf.contentValues()
-                       if content.get_state(o) == 'published']
-
-        return self.index()
-
-    def getDescription(self, image):
-        return image.long_description.output
-
-    def getTitle(self, image):
-        return image.title
-
-    def getImageUrl(self, image):
-        return image.absolute_url()
-
 
 class GoPDB(BrowserView):
     def __call__(self):
