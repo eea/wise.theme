7f5d8516c9709f8af9be6af123ce17eb83ba632d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-03-05 13:27:36 +0200

    Added csv with stats

--- src/wise/theme/browser/countryfactsheets.py
+++ src/wise/theme/browser/countryfactsheets.py
@@ -1,14 +1,80 @@
+import csv
 import json
+from collections import namedtuple
+from decimal import Decimal
 
 import requests
+from pkg_resources import resource_filename
+from zope.i18n.locales import locales
 
 from Products.Five.browser import BrowserView
+
+Stat = namedtuple('Stat', ['Country', 'Subregion', 'Area_km2', 'Type'])
+
+
+def parse_csv():
+    wf = resource_filename('wise.theme', 'data/Marine_waters_statistics.csv')
+
+    reader = csv.reader(open(wf))
+    cols = reader.next()
+    stats = []
+
+    for line in reader:
+        d = dict(zip(cols, line))
+        s = Stat(**d)
+        stats.append(s)
+
+    return stats
+
+
+STATS = parse_csv()
 
 
 class CountryFactsheetView(BrowserView):
     """ Country factsheet view
     """
-    pass
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    regions = {
+        "BAL": "Baltic Sea",
+        "ATL": "North-East Atlantic Ocean",
+        "ANS": "NE Atlantic: Greater North Sea, incl. Kattegat & English Channel",
+        "ACS": "NE Atlantic: Celtic Seas",
+        "ABI": "NE Atlantic: Bay of Biscay & Iberian Coast",
+        "AMA": "NE Atlantic: Macaronesia",
+        "MED": "Mediterranean Sea",
+        "MWE": "Western Mediterranean Sea",
+        "MAD": "Mediterranean: Adriatic Sea",
+        "MIC": "Mediterranean: Ionian Sea & Central Mediterranean Sea",
+        "MAL": "Mediterranean: Aegean - Levantine Sea",
+        "BLK": "Black Sea",
+    }
+
+    def format_nr(self, nr):
+        target_language = 'en'
+        locale = locales.getLocale(target_language)
+        formatter = locale.numbers.getFormatter('decimal')
+
+        return formatter.format(Decimal(str(nr)))
+
+    def get_subregion_title(self, region_id):
+        return self.regions.get(region_id, region_id)
+
+    def water_stats(self):
+        code = self.context.country
+        res = []
+
+        for stat in STATS:
+            if stat.Country == code:
+                res.append([self.regions[stat.Subregion], stat.Area_km2])
+
+        return res
+
+    def water_stats_total(self):
+        return sum([int(b[1]) for b in self.water_stats()])
 
 
 GET_EXTENT_URL = (
