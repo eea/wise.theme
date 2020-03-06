e810931f76c06348d614922d3d0d711864ef5c13
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-03-06 14:47:45 +0200

    Implement competent authorities

--- src/wise/theme/browser/countryfactsheets.py
+++ src/wise/theme/browser/countryfactsheets.py
@@ -5,15 +5,21 @@ from decimal import Decimal
 
 import requests
 from pkg_resources import resource_filename
+from zope.component import getUtility
 from zope.i18n.locales import locales
+from zope.schema.interfaces import IVocabularyFactory
 
 from Products.Five.browser import BrowserView
 
 Stat = namedtuple('Stat', ['Country', 'Subregion', 'Area_km2', 'Type'])
+ConvWebsite = namedtuple('ConvWebsite', ['RSC', 'Web'])
+CountryConv = namedtuple('CountryConv', ['Country', 'RSCs'])
+MSFDWebsites = namedtuple('MSFDWebsites', ['Country', 'URL', 'Observations'])
+Website = namedtuple('Website', ['name', 'href'])
 
 
-def parse_csv():
-    wf = resource_filename('wise.theme', 'data/Marine_waters_statistics.csv')
+def parse_csv(path, klass):
+    wf = resource_filename('wise.theme', path)
 
     reader = csv.reader(open(wf))
     cols = reader.next()
@@ -21,13 +27,16 @@ def parse_csv():
 
     for line in reader:
         d = dict(zip(cols, line))
-        s = Stat(**d)
+        s = klass(**d)
         stats.append(s)
 
     return stats
 
 
-STATS = parse_csv()
+STATS = parse_csv('data/Marine_waters_statistics.csv', Stat)
+CONVENTION_WEBSITES = parse_csv('data/convention_websites.csv', ConvWebsite)
+COUNTRY_CONVENTIONS = parse_csv('data/country_conventions.csv', CountryConv)
+MSFD_WEBSITES = parse_csv('data/MSFD_websites.csv', MSFDWebsites)
 
 
 class CountryFactsheetView(BrowserView):
@@ -75,6 +84,59 @@ class CountryFactsheetView(BrowserView):
 
     def water_stats_total(self):
         return sum([Decimal(b[1]) for b in self.water_stats()])
+
+    def msfd_website(self):
+        for line in MSFD_WEBSITES:
+            if line.Country == self.context.country:
+                return line
+
+    def conventions(self):
+        res = []
+
+        for line in COUNTRY_CONVENTIONS:
+            if line.Country == self.context.country:
+                convs = line.RSCs.split(';')
+
+                for conv_name in convs:
+                    for cw in CONVENTION_WEBSITES:
+                        if cw.RSC == conv_name:
+                            res.append(cw)
+
+        return res
+
+    def authorities(self):
+        from wise.msfd.data import _get_report_filename_art7_2018
+        from wise.msfd.data import get_report_file_url
+        from wise.msfd.data import get_xml_report_data
+        from lxml.etree import fromstring
+
+        res = []
+        fname = _get_report_filename_art7_2018(self.context.country,
+                                               None, None, None)
+        # furl = get_report_file_url(fname)
+        report_data = get_xml_report_data(fname)
+        etree = fromstring(report_data)
+        NSMAP = {"w": "http://water.eionet.europa.eu/schemas/dir200856ec"}
+        auth_nodes = etree.xpath('//w:CompetentAuthority', namespaces=NSMAP)
+
+        for node in auth_nodes:
+            name = node.xpath('w:CompetentAuthorityName/text()',
+                              namespaces=NSMAP)[0]
+            href = node.xpath('w:URL/text()', namespaces=NSMAP)[0]
+
+            res.append(Website(name, href))
+
+        return res
+
+    def country_name(self):
+        util = getUtility(IVocabularyFactory, name="wise_search_member_states")
+        vocab = util(self.context)
+
+        for term in vocab:
+            if term.value == self.context.country:
+                return term.title
+
+        return ''
 
 
 GET_EXTENT_URL = (
