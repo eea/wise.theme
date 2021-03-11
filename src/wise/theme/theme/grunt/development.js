45794966a551ee5688836dec0877d0bc487860fd
kreafox <krisztina.n.elekes@gmail.com>
2021-03-11 09:54:12 +0200

    Add custom download option for dashboards

--- src/wise/theme/theme/grunt/development.js
+++ src/wise/theme/theme/grunt/development.js
@@ -2,22 +2,22 @@ module.exports = {
   less: {
     development: {
       options: {
-          paths: [],
-          strictMath: false,
-          sourceMap: true,
-          outputSourceFiles: true,
-          sourceMapFileInline: true,
-          sourceMapURL: '++theme++wise-theme/less/theme-compiled.less.map',
-          sourceMapFilename: 'less/theme-compiled.less.map',
-          modifyVars: {
-              "isPlone": "false"
-          }
+        paths: [],
+        strictMath: false,
+        sourceMap: true,
+        outputSourceFiles: true,
+        sourceMapFileInline: true,
+        sourceMapURL: "++theme++wise-theme/less/theme-compiled.less.map",
+        sourceMapFilename: "less/theme-compiled.less.map",
+        modifyVars: {
+          isPlone: "false",
+        },
       },
       files: {
-        'static/css/theme-compiled.css': 'less/theme.local.less',
-        'static/css/critical.css': 'less/critical.less',
-      }
-    }
+        "static/css/theme-compiled.css": "less/theme.local.less",
+        "static/css/critical.css": "less/critical.less",
+      },
+    },
   },
 
   // copy: {
@@ -37,32 +37,26 @@ module.exports = {
   concat: {
     scripts: {
       files: {
-        'static/js/theme-compiled.js': [
-          'js/**/*.js',
-        ],
-      }
-    }
+        "static/js/theme-compiled.js": ["js/**/*.js"],
+      },
+    },
   },
 
   watch: {
     styles: {
-      files: [
-          'less/*.less',
-          'barceloneta/less/*.less',
-          'custom/less/*.less'
-      ],
-      tasks: ['less:development'],
+      files: ["less/*.less", "barceloneta/less/*.less", "custom/less/*.less"],
+      tasks: ["less:development"],
       options: {
-        nospawn: true
-      }
+        nospawn: true,
+      },
     },
     scripts: {
-      files: ['js/**/*.js'],
-      tasks: ['concat'],
+      files: ["js/**/*.js"],
+      tasks: ["concat"],
       // tasks: ['copy'],
       options: {
-        nospawn: true
-      }
-    }
-  }
+        nospawn: true,
+      },
+    },
+  },
 };
--- src/wise/theme/theme/grunt/production.js
+++ src/wise/theme/theme/grunt/production.js
@@ -1,47 +1,44 @@
 module.exports = {
-
   less: {
     production: {
       options: {
         compress: true,
-        sourceMap: false
+        sourceMap: false,
       },
       files: {
-          'static/css/theme-compiled.css': 'less/theme.local.less',
-          'static/css/critical.css': 'less/critical.less',
-      }
-    }
+        "static/css/theme-compiled.css": "less/theme.local.less",
+        "static/css/critical.css": "less/critical.less",
+      },
+    },
   },
 
   postcss: {
-      options: {
-          map: true,
-          processors: [
-              require('autoprefixer')({
-                  browsers: ['last 2 versions']
-              })
-          ]
-      },
-      dist: {
-          src: 'less/*.css'
-      }
+    options: {
+      map: true,
+      processors: [
+        require("autoprefixer")({
+          browsers: ["last 2 versions"],
+        }),
+      ],
+    },
+    dist: {
+      src: "less/*.css",
+    },
   },
 
   concat: {
     scripts: {
       files: {
-        'static/js/theme-compiled.js': [
-          'js/**/*.js',
-        ],
-      }
-    }
+        "static/js/theme-compiled.js": ["js/**/*.js"],
+      },
+    },
   },
 
   uglify: {
     scripts: {
       files: {
-        'static/js/theme-compiled.js': ['static/js/theme-compiled.js']
-      }
-    }
-  }
+        "static/js/theme-compiled.js": ["static/js/theme-compiled.js"],
+      },
+    },
+  },
 };
