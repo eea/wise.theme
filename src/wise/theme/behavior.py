d58c2768bee29b405231b0aff6fee4da9e6c6190
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-04-06 17:12:51 +0300

    Add behavior

--- src/wise/theme/behavior.py
+++ src/wise/theme/behavior.py
@@ -1,7 +1,8 @@
 from plone.app.dexterity.behaviors.metadata import (DCFieldProperty,
                                                     MetadataBase)
 
-from .interfaces import IDisclaimer, IExternalLinks, IReferenceLinks
+from .interfaces import (ICatalogueMetadata, IDisclaimer, IExternalLinks,
+                         IReferenceLinks)
 
 
 class ExternalLinks(MetadataBase):
@@ -20,3 +21,13 @@ class Disclaimer(MetadataBase):
     """Disclaimer Behavior"""
 
     disclaimer = DCFieldProperty(IDisclaimer["disclaimer"])
+
+
+class CatalogueMetadata(MetadataBase):
+    """Wise metadata"""
+
+    organisation = DCFieldProperty(ICatalogueMetadata["organisation"])
+    dpsir_type = DCFieldProperty(ICatalogueMetadata["dpsir_type"])
+    theme = DCFieldProperty(ICatalogueMetadata["theme"])
+    subtheme = DCFieldProperty(ICatalogueMetadata["subtheme"])
+    publication_year = DCFieldProperty(ICatalogueMetadata["publication_year"])
