63225127bb0aeeeeb5c7789f979ecbbdae7d2328
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-04-20 15:09:00 +0300

    Add remote fetch data button for original source

--- src/wise/theme/behavior.py
+++ src/wise/theme/behavior.py
@@ -26,9 +26,12 @@ class Disclaimer(MetadataBase):
 class CatalogueMetadata(MetadataBase):
     """Wise metadata"""
 
+    original_source = DCFieldProperty(ICatalogueMetadata["original_source"])
     organisation = DCFieldProperty(ICatalogueMetadata["organisation"])
     dpsir_type = DCFieldProperty(ICatalogueMetadata["dpsir_type"])
     theme = DCFieldProperty(ICatalogueMetadata["theme"])
     subtheme = DCFieldProperty(ICatalogueMetadata["subtheme"])
     publication_year = DCFieldProperty(ICatalogueMetadata["publication_year"])
     thumbnail = DCFieldProperty(ICatalogueMetadata["thumbnail"])
+
+    sources = DCFieldProperty(ICatalogueMetadata["sources"])
