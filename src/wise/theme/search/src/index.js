7fd6f8e9b2b01c46e7a3317bdbcd28690fb77db6
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-08-04 11:28:15 +0300

    Added watch script

--- src/wise/theme/search/src/index.js
+++ src/wise/theme/search/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {SearchApp, registry} from '@eeacms/search';
-import reportWebVitals from './reportWebVitals';
 import installConfig from './config';
 
 import '@elastic/react-search-ui-views/lib/styles/styles.css';
-
-import './semantic-ui.less';
+import "@eeacms/search-less/dist/main.css";
 
 // ++resource++measures-catalogue/
 // import './index.css';
 
 const configRegistry = installConfig(registry);
+console.log('here 2');
 
 ReactDOM.render(
     <SearchApp registry={configRegistry} appName="wise" />,
@@ -21,4 +20,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import reportWebVitals from './reportWebVitals';
+// reportWebVitals();
