21b938897d7c47b98172a21f959ed6619575d5c6
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-08-09 16:04:57 +0300

    Update indexer

--- src/wise/theme/search/data/importer.py
+++ src/wise/theme/search/data/importer.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 import csv
+import json
 import os
 from collections import defaultdict
+
+import tqdm
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
+
+OM = 'Origin of the measure'
 
 
 def read_details_csv_files(location):
@@ -50,18 +57,125 @@ def read_master_csv_files(location):
     return data
 
 
-OM = 'Origin of the measure'
+def fix_descriptor(rec):
+    descriptors = [f'D{n}' for n in range(1, 12)]
+    s = []
+    for d in descriptors:
+        if rec[d] == '1':
+            s.append(d)
+        del rec[d]
+    rec['Descriptors'] = s
+
+
+def fix_impacts(rec):
+    s = []
+    fields = [
+        'IMPACTS Waste management', 'IMPACTS Air pollution',
+        'IMPACTS Marine litter', 'IMPACTS NIS', 'IMPACTS Noise',
+        'IMPACTS Pollution', 'IMPACTS Water pollution', 'IMPACTS Other'
+    ]
+    for f in fields:
+        if f in rec:
+            if rec[f] == '1':
+                s.append(f.replace('IMPACTS ', ''))
+            del rec[f]
+    rec['Impacts'] = s
+
+
+def fix_keywords(rec):
+    s = []
+
+    if 'Keywords' in rec:
+        rec['Keywords'] = list(
+            filter(None, [k.strip() for k in rec['Keywords'].split(',')]))
+        return
+
+    fields = [
+        'accident management', 'administrative', 'air pollution',
+        'anchoring/mooring', 'awareness raising', 'ballast waters',
+        'construction', 'dredging', 'EU policies', 'hull fouling',
+        'international agreements', 'legislation/regulation', 'maintenence',
+        'marine litter', 'navigation', 'NIS', 'noise', 'pollution',
+        'regional sea convention', 'PSSA/ZMES', 'technical measures',
+        'waste management', 'water pollution'
+    ]
+    for f in fields:
+        if f in rec:
+            if rec[f] == '1':
+                s.append(f)
+            del rec[f]
+    rec['Keywords'] = s
+
+
+def get_id(rec):
+    if rec.get('MeasureCode'):
+        return rec['MeasureCode']
+    if rec.get('CodeCatalogue'):
+        return rec['CodeCatalogue']
 
 
 def main():
+    host = 'localhost'
+    index = 'wise_catalogue_measures'
+
+    conn = Elasticsearch([host])
     master_data = read_master_csv_files('./csv')
+
     data = read_details_csv_files('./csv')
 
     cursors = defaultdict(lambda: 0)
     for (i, line) in enumerate(master_data):
         measure_name = line[OM]
-        master_data[i].update(data[measure_name][cursors[OM]])
+        rec = data[measure_name][cursors[measure_name]]
+        master_rec = master_data[i]
+        keys = master_rec.keys()
+        for k, v in rec.items():
+            for mk in keys:
+                if k.lower().strip() == mk.lower().strip():
+                    k = mk
+            master_rec[k] = v
+
+        fix_descriptor(master_rec)
+        fix_impacts(master_rec)
+        fix_keywords(master_rec)
+
         cursors[measure_name] += 1
+        _id = get_id(master_rec)
+        master_rec['_id'] = _id
+        master_rec['_index'] = index
+
+    ids = set([rec['_id'] for rec in master_data])
+    print(len(ids))
+
+    body = []
+    for doc in master_data:
+        body.append(json.dumps({"create": doc}))
+
+    print(f"Indexing {len(master_data)} documents")
+    num_docs = len(master_data)
+    progress = tqdm.tqdm(unit="docs", total=num_docs)
+
+    successes = 0
+
+    for ok, action in streaming_bulk(
+        client=conn, index=index, actions=iter(master_data),
+    ):
+        progress.update(1)
+        successes += ok
+
+    print("Indexed %d/%d documents" % (successes, num_docs))
+
+    # jsonl = '\n'.join(body)
+    # with open('out.jsonl', 'w') as f:
+    #     f.write(jsonl)
+
+    # import pdb
+    # pdb.set_trace()
+    # conn.bulk(jsonl, index=index)
+
+    # for i, doc in enumerate(master_data):
+    # conn.index(index="wise", id=uid, body=doc)
+    # print(f"Index #{i}: {uid}")
 
 
 if __name__ == "__main__":
