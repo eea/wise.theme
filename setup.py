fa45ec8bfbcfd25a6c4620ed2a8fd9f7a434c91c
laszlocseh <csehlaszlo91@gmail.com>
2024-07-26 12:09:03 +0300

    added packages in setup.py

--- setup.py
+++ setup.py
@@ -54,6 +54,10 @@ setup(
         'webcouturier.dropdownmenu',
         'robotsuite',
         'wise.msfd@git+ssh://git@github.com:eea/wise.msfd.git',
+        'pyexcel==0.6.7',
+        'pyexcel-xlsx==0.6.0',
+        'openpyxl==3.0.10',
+        'pdfkit',
     ],
     extras_require={
         'test': [
@@ -64,10 +68,6 @@ setup(
             'plone.testing>=5.0.0',
             'plone.app.contenttypes',
             'plone.app.robotframework[debug]',
-            'pdfkit',
-            'pyexcel==0.6.7',
-            'pyexcel-xlsx==0.6.0',
-            'openpyxl==3.0.10',
         ],
     },
     entry_points="""
